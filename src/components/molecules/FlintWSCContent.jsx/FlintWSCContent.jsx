f671f6773ec66b8adc818ff12fe5b7e012f334f6
paulclindo <paulclindo@gmail.com>
2023-06-07 19:48:15 -0500

    wip: header content

--- src/components/molecules/FlintWSCContent.jsx/FlintWSCContent.jsx
+++ src/components/molecules/FlintWSCContent.jsx/FlintWSCContent.jsx
@@ -100,6 +100,33 @@ const FlintWSCContent = () => {
 
   return (
     <div className="content">
+      <div className="header">
+        <span>Cardano</span>
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 512 512"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+        </svg>
+        <span>WSC</span>
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 512 512"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+        </svg>
+        <span>Dapp</span>
+      </div>
       <Tabs>
         <Tabs.TabPane tab="Cardano" key="cardano">
           <CardanoAssets
