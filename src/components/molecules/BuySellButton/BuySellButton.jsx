e6f8d500a56aad7ace2fdb646c3bc68d9b5bf69e
paulclindo <paulclindo@gmail.com>
2023-06-14 19:31:15 -0500

    feat: add modal with stepper

--- src/components/molecules/BuySellButton/BuySellButton.jsx
+++ src/components/molecules/BuySellButton/BuySellButton.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import CustomButton from "../../atoms/CustomButton/CustomButton";
 import { ArrowRightOutlined } from "@ant-design/icons";
+import { useAppProvider } from "../../../context/AppProvider";
+import BuySellWSCButton from "../BuySellWSCButton/BuySellWSCButton";
 
 const capitalizeString = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string?.charAt(0).toUpperCase() + string?.slice(1);
 };
 
 const BuySellButton = ({ onClick, buyOrSell, currencyName, disabled }) => {
+  const { activeConnector } = useAppProvider();
+
+  if (activeConnector?.id?.includes("wsc")) {
+    return (
+      <BuySellWSCButton
+        text={`${capitalizeString(buyOrSell)} ${currencyName}`}
+        variant="primary"
+        icon={<ArrowRightOutlined />}
+        onClick={onClick}
+        // disabled={disabled}
+      />
+    );
+  }
+
   return (
     <CustomButton
       type="submit"
