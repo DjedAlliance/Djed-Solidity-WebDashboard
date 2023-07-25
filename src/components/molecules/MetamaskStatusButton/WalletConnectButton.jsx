f26c4692d55ed55f3c0f833fb7d63aa64f40068b
paulclindo <paulclindo@gmail.com>
2023-07-25 12:53:45 -0500

    fix UI

--- src/components/molecules/MetamaskStatusButton/WalletConnectButton.jsx
+++ src/components/molecules/MetamaskStatusButton/WalletConnectButton.jsx
@@ -8,7 +8,6 @@ import { useNavigate } from "react-router-dom";
 import "./_MetamaskStatusButton.scss";
 import { useDisconnect } from "wagmi";
 import { Button, Popover } from "antd";
-import FlintWSCContent, { WSCWalletLink } from "../FlintWSCContent.jsx/FlintWSCContent";
 import { WSCInterface } from "milkomeda-wsc-ui-test-beta";
 
 const WalletConnectButton = () => {
@@ -30,16 +29,40 @@ const WalletConnectButton = () => {
 
   return (
     <div className="wrapper-connect-btn">
-      {/*TODO: adapt UI on mobile*/}
-
       <Popover
         getPopupContainer={(trigger) => trigger.parentElement}
         content={
           isWalletConnected ? (
             <>
-              {activeConnector?.id?.includes("wsc") && <WSCInterface />}
+              {activeConnector?.id?.includes("wsc") && (
+                <div className="only-desktop">
+                  <WSCInterface />
+                </div>
+              )}
               <div className="only-mobile">
-                <WSCWalletLink />
+                <a
+                  className="external-link"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  href="https://wsc-wallet-dev.milkomeda.com/"
+                >
+                  <span>Go to WSC Wallet Page</span>
+                  <svg
+                    stroke="currentColor"
+                    fill="none"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                    <polyline points="15 3 21 3 21 9"></polyline>
+                    <line x1="10" y1="14" x2="21" y2="3"></line>
+                  </svg>
+                </a>
               </div>
               <DisconnectButton />
             </>
