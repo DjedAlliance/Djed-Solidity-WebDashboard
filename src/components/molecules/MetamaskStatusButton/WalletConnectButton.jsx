60b8d574c1d33b768f2f8bd495defe73abbf166b
paulclindo <paulclindo@gmail.com>
2023-10-05 18:50:53 -0500

    remove popup

--- src/components/molecules/MetamaskStatusButton/WalletConnectButton.jsx
+++ src/components/molecules/MetamaskStatusButton/WalletConnectButton.jsx
@@ -8,7 +8,6 @@ import { useNavigate } from "react-router-dom";
 import "./_MetamaskStatusButton.scss";
 import { useDisconnect } from "wagmi";
 import { Button, Popover } from "antd";
-import { WSCInterface } from "milkomeda-wsc-ui-test-beta";
 
 const WalletConnectButton = () => {
   const {
@@ -29,8 +28,7 @@ const WalletConnectButton = () => {
     redirectToFlint,
     redirectToEternl,
     redirectToNami,
-    redirectToNufi,
-    activeConnector
+    redirectToNufi
   } = useAppProvider();
 
   return (
@@ -39,39 +37,7 @@ const WalletConnectButton = () => {
         getPopupContainer={(trigger) => trigger.parentElement}
         content={
           isWalletConnected ? (
-            <>
-              {activeConnector?.id?.includes("wsc") && (
-                <div className="only-desktop">
-                  <WSCInterface />
-                </div>
-              )}
-              <div className="only-mobile">
-                <a
-                  className="external-link"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  href="https://wsc-wallet-dev.milkomeda.com/"
-                >
-                  <span>Go to WSC Wallet Page</span>
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                    <polyline points="15 3 21 3 21 9"></polyline>
-                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                  </svg>
-                </a>
-              </div>
-              <DisconnectButton />
-            </>
+            <DisconnectButton />
           ) : (
             <>
               <CustomButton
