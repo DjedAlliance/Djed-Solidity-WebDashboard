bee71247fc3eabbe46e2f4f0215671703371c37c
Ronnieraj37 <101713157+Ronnieraj37@users.noreply.github.com>
2024-04-12 09:55:06 +0530

    Removed conflicts from other repo

--- craco.config.js
+++ craco.config.js
@@ -29,17 +29,6 @@ module.exports = {
         })
       );
 
-      webpackConfig.ignoreWarnings = [
-        function ignoreSourcemapsloaderWarnings(warning) {
-          return (
-            warning.module &&
-            warning.module.resource.includes("node_modules") &&
-            warning.details &&
-            warning.details.includes("source-map-loader")
-          );
-        },
-      ];
-
       return webpackConfig;
     }
   }
